3d5f3e6602fc529ed9f0e5b066526a64bd431845
ChiragBellara <chiragbellara7@gmail.com>
2025-11-15 10:44:38 -0800

    Migrating to LangChain 1.0. Created a Playground folder to test everything and adapt to the project.

--- api/agents/agent.py
+++ api/agents/agent.py
@@ -1,12 +1,13 @@
-from agents.tools import fetch_weather
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from agents.prompt import SYSTEM_PROMPT
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from agents.history import get_chat_history
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, ToolMessage, SystemMessage
-import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents import create_agent
+from agents.tools import fetch_weather
 
 load_dotenv(override=True)
 
@@ -18,7 +19,26 @@ def create_stars_agent():
     """Create the Stars agent with Streamlit-integrated memory"""
 
     try:
-        system_prompt = SystemMessage(cotent=SYSTEM_PROMPT)
+        llm = ChatOpenAI(
+            model="gpt-4o-mini",
+            temperature=0.7,
+            timeout=60,
+            max_retries=2
+        )
+
+        tools = [fetch_weather]
+
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", SYSTEM_PROMPT),
+            # where memory will be injected
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+            # where tool thoughts/actions go
+            MessagesPlaceholder("agent_scratchpad"),
+        ])
+
+        agent = create_agent(llm, tools=tools, system_prompt=SYSTEM_PROMPT)
+
     except Exception as e:
         st.error(f"Failed to create agent: {str(e)}")
         return None
